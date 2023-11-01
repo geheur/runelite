d01cdb554898c365281573e25cb08a10ad94bae8
sam <dasistkeinnamen@gmail.com>
2023-10-31 19:02:25 -0700

    Fix being unable to remove hidden or highlighted items from ground items if those items had a quantity.

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/ItemThreshold.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/ItemThreshold.java
@@ -94,4 +94,17 @@ class ItemThreshold
 			return itemCount > quantity;
 		}
 	}
+
+	public static boolean matchesEntry(String entry, String itemName) {
+		int index = entry.indexOf('<');
+		if (index == -1)
+		{
+			index = entry.indexOf('>');
+		}
+		if (index != -1)
+		{
+			entry = entry.substring(0, index);
+		}
+		return itemName.equalsIgnoreCase(entry);
+	}
 }
